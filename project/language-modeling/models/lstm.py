dc8808e86629517beed14a573294499653145ebf
filippodaniotti <febbrosto@gmail.com>
2023-09-11 16:36:56 +0200

    discard bidirectional lstm

--- project/language-modeling/models/lstm.py
+++ project/language-modeling/models/lstm.py
@@ -24,10 +24,8 @@ class BaselineLSTM(nn.Module):
             embedding_dim,
             hidden_dim,
             num_layers=num_layers,
-            # bidirectional=True,
             batch_first=True,)
         self.fc = nn.Linear(hidden_dim, num_classes)
-        # self.fc = nn.Linear(hidden_dim*2, num_classes)
 
         self.dropout = nn.Dropout(p_dropout) if p_dropout is not None else None
         self.norm = nn.LayerNorm(embedding_dim) if layer_norm else None
@@ -45,11 +43,6 @@ class BaselineLSTM(nn.Module):
             outputs = self.norm(outputs)
         if self.dropout is not None:
             outputs = self.dropout(outputs)
-        # forward_outputs, backward_outputs = split(outputs, split_size_or_sections=self.hidden_dim, dim=2)
-        # forward_preds = self.fc(forward_outputs)
-        # forward_preds = forward_preds.reshape(-1, forward_preds.shape[2])
-        # backward_preds = self.fc(backward_outputs)
-        # backward_preds = backward_preds.reshape(-1, backward_preds.shape[2])
         preds = self.fc(outputs)
         preds = preds.reshape(-1, preds.shape[-1])
         return preds, (hidden, cell)
